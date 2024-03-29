b2cdea009146149a8cb9f7c345653aeb9b0f6983
Clarisse <clarisse421@berkeley.edu>
2024-03-29 16:19:22 -0400

    Adjusting the layout for desktop version

--- src/components/3-BenchWithArt.js
+++ src/components/3-BenchWithArt.js
@@ -1,15 +1,6 @@
 import benchImage from "../assets/mobile/image-grid-2.jpg";
 function BenchWithArt() {
-  return (
-    <div className="container">
-      <div className="BenchWithArt row">
-        <img
-          src={benchImage}
-          alt="Empty art room exhibit with a wooden bench in the foreground with a piece of art in the background on the wall"
-        ></img>
-      </div>
-    </div>
-  );
+  return <></>;
 }
 
 export default BenchWithArt;
--- src/components/4-ComeAndBeInspired.js
+++ src/components/4-ComeAndBeInspired.js
@@ -1,23 +1,6 @@
 import HallwayArt from "../assets/mobile/image-grid-3.jpg";
 function ComeAndBeInspired() {
-  return (
-    <div className="container">
-      <div className="ComeAndBeInspired row">
-        <img
-          src={HallwayArt}
-          alt="Art gallery hallway with people looking at the art"
-          className="col"
-        ></img>
-        <div className="box col">
-          <h2>COME & BE INSPIRED</h2>
-          <p>
-            We’re excited to welcome you to our gallery and see how our
-            collections influence you.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+  return <></>;
 }
 
 export default ComeAndBeInspired;
