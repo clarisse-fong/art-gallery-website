bd897229b8c337330400c6f73913efd08afb9253
Clarisse <clarisse421@berkeley.edu>
2024-04-03 22:48:47 -0400

    Finished footer for mobile and desktop versions

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import "./Footer.scss";
+import Facebook from "../../assets/icon-facebook.svg";
+import Instagram from "../../assets/icon-instagram.svg";
+import Twitter from "../../assets/icon-twitter.svg";
+import LogoLight from "../../assets/logo-light.svg";
+
+function Footer() {
+  return (
+    <div className="Footer container-fluid">
+      <img className="logo" src={LogoLight} alt="The Modern Art logo"></img>
+      <p className="description small-body-font">
+        The Modern Art Gallery is free to all visitors and open seven days a
+        week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
+      </p>
+      <div className="social-media">
+        <img className="FB" src={Facebook} alt="Facebook Logo"></img>
+        <img className="IG" src={Instagram} alt="Instagram Logo"></img>
+        <img className="Twitter" src={Twitter} alt="Twitter Logo"></img>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
